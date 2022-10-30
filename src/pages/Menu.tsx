d88472f91d55e37666fab6af858f61bcf12889a4
takatokomori <ktakato510@gmail.com>
2022-10-30 09:17:21 +0900

    create teh menu page

---
+++ src/pages/Menu.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../styles/Menu.css";
+
+export default function Menu() {
+  return (
+    <div className='menu'>
+        <h1 className='menuTitle'>Our Menu</h1>
+    </div>
+  )
+}
